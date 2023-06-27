5710f9776393b5f7e68bd5cacb9f892a80d4154a
F3riko <vyrydy@mail.ru>
2023-06-27 18:33:03 +0300

    Movie component has been created, basic functionality was added to the App.js

---
+++ src/components/Movies.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+
+const Movie = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=54afa970f66b8f17236a011ce7bf1603`
+        );
+        const data = await response.json();
+        setMovies(data.results);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Movies</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Movie;
